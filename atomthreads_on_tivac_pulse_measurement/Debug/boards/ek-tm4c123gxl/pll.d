61fce081a1352464cfc3068cf88e6bd48a9d6f92
Vishnu Gumme <31954135+vishnugumme@users.noreply.github.com>
2021-02-07 14:22:41 +0530

    Delete atomthreads_on_tivac_pulse_measurement directory

--- atomthreads_on_tivac_pulse_measurement/Debug/boards/ek-tm4c123gxl/pll.d
+++
@@ -1,7 +0,0 @@
-boards/ek-tm4c123gxl/pll.o: ../boards/ek-tm4c123gxl/pll.c \
- /home/saish/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h \
- ../boards/ek-tm4c123gxl/pll.h
-
-/home/saish/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h:
-
-../boards/ek-tm4c123gxl/pll.h:
--- atomthreads_on_tivac_pulse_measurement/Debug/boards/ek-tm4c123gxl/systick.d
+++
@@ -1,4 +0,0 @@
-boards/ek-tm4c123gxl/systick.o: ../boards/ek-tm4c123gxl/systick.c \
- /home/saish/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
-
-/home/saish/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h:
